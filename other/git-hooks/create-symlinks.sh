18fc50f507d355e344148559c96195ed1ef9af7f
michael maindal jensen <59361496+lobsterlooks@users.noreply.github.com>
2020-05-18 18:21:28 +0200

    Update
    Update

--- other/git-hooks/create-symlinks.sh
+++ other/git-hooks/create-symlinks.sh
@@ -1,5 +1,5 @@
 #!/bin/sh
 
 ln -s -f other/git_hooks/pre-commit .git/hooks/pre-commit
-ln -s -f other/git_hooks/check-git-commit.py .git/hooks/check-git-Github.py
+ln -s -f other/git_hooks/check-git-next.py .git/hooks/check-git-Github.py
 ln -s -f other/git_hooks/commit-msg .git/hooks/commit-msg
